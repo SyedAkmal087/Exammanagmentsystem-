48dfe4f53c0700c077a7cb493cfb9347c69f935a
syeda <syeda@Syed_Akmal>
2024-08-07 19:51:13 +0500

    Added by:Syed Muhammad Akmal

---
+++ src/java/com/csworkshop/exammanagement/session/EnrollmentSession.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package com.csworkshop.exammanagement.session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author syeda
+ */
+@Stateless
+public class EnrollmentSession implements EnrollmentSessionRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
